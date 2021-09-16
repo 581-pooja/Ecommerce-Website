aaa824513a187e5afda4cef26ce962d9fc33937b
Pooja Bhagat <PoojaBhagat5801@gmail.com>
2021-09-17 01:22:57 +0530

    Added the bootsrap ,templates and Static folder and included the layout of the website in urls.py and views.py i.e. paths

--- mac/shop/urls.py
+++ mac/shop/urls.py
@@ -4,4 +4,11 @@ from . import views
 urlpatterns = [
     # from mac.urls comes to views , from views for " " goes to views.index
     path("", views.index, name="ShopHome"),
+    path("about/", views.about, name="AboutUs"),
+    path("contact/", views.contact, name="ContactUS"),
+    path("tracker/", views.tracker, name="Tracking Status"),
+    path("search/", views.search, name="Search"),
+    path("productView/", views.productView, name="ProductView"),
+    path("checkout/", views.checkout, name="CheckOut"),
+
 ]
--- mac/shop/views.py
+++ mac/shop/views.py
@@ -3,4 +3,23 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Index of Shop Page!!")
+    # return HttpResponse("Index of Shop Page!!")
+    return render(request, 'shop/index.html')
+
+def about(request):
+    return HttpResponse("About of Shop Page!!")
+
+def contact(request):
+    return HttpResponse("Contact of Shop Page!!")
+
+def tracker(request):
+    return HttpResponse("Tracker of Shop Page!!")
+
+def search(request):
+    return HttpResponse("Search of Shop Page!!")
+
+def productView(request):
+    return HttpResponse("Product View of Shop Page!!")
+
+def checkout(request):
+    return HttpResponse("Checkout of Shop Page!!")
