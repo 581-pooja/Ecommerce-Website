97e9935bf83dbfdcc7b0937f3071a9d338a6c554
Pooja Bhagat <PoojaBhagat5801@gmail.com>
2021-09-23 00:55:02 +0530

    1. Added Sliders for products 2. Added cart status in navigation bar including popover 3. created remaing templates pages

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,26 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 # Create your views here.
 def index(request):
-    # return HttpResponse("Index of Shop Page!!")
-    return render(request, 'shop/index.html')
+    # products = Product.objects.all()
+    # print(products)
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    # print("Catprods: " , catprods)
+
+    cats = {item['category'] for item in catprods}
+    # print("cats: " , catprods)
+
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        # print(prod)
+        n = len(prod)
+        # print(n)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+        # print(allProds)
+    params = {'allProds': allProds}
+    return render(request, 'shop/index.html', params)
+
 
 def about(request):
     return render(request, 'shop/about.html')
-    # return HttpResponse("About of Shop Page!!")
-
+    
 def contact(request):
-    return HttpResponse("Contact of Shop Page!!")
+    return render(request, 'shop/contact.html')
 
 def tracker(request):
-    return HttpResponse("Tracker of Shop Page!!")
+    return render(request, 'shop/tracker.html')
 
 def search(request):
-    return HttpResponse("Search of Shop Page!!")
+    return render(request, 'shop/search.html')
+    # return HttpResponse("Search of Shop Page!!")
 
 def productView(request):
     return HttpResponse("Product View of Shop Page!!")
 
 def checkout(request):
-    return HttpResponse("Checkout of Shop Page!!")
+    return render(request, 'shop/checkout.html')
