f7e56b7a131f4a45af9a9977e6da16e0cfaf22b1
Pooja Bhagat <PoojaBhagat5801@gmail.com>
2021-09-26 01:27:18 +0530

    Successfully added contact page after hours of debugging and errors

--- mac/shop/models.py
+++ mac/shop/models.py
@@ -9,8 +9,19 @@ class Product(models.Model):
     subcategory = models.CharField(max_length=50, default="")
     price = models.IntegerField(default=0)
     desc = models.CharField(max_length=1000)
-    pub_data = models.DateField()
+    pub_date = models.DateField()
     image = models.ImageField(upload_to = "shop/images",default="")
 
     def __str__(self):
         return self.product_name
+
+class Contact(models.Model):
+    msg_id = models.AutoField(primary_key=True)
+    name  = models.CharField(max_length=50 , default="")
+    email  = models.CharField(max_length=254 , default="")
+    phone = models.CharField(max_length=50,default="")
+    desc  = models.CharField(max_length=500 , default="")
+    msg_date = models.DateField()
+
+    def __str__(self):
+        return self.name
--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product, Contact
 from math import ceil
+import datetime
 
 # Create your views here.
 def index(request):
@@ -30,6 +31,16 @@ def about(request):
     return render(request, 'shop/about.html')
     
 def contact(request):
+    if request.method == "POST":
+        # print(request)
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        desc = request.POST.get('desc', '')
+        msg_date= datetime.datetime.now()
+        # print(name,email,phone,desc,msg_date)
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, msg_date= msg_date)
+        contact.save()
     return render(request, 'shop/contact.html')
 
 def tracker(request):
