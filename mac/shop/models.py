56e491f101f31ed91e4d37efec28ddeb7a22037e
Pooja Bhagat <PoojaBhagat5801@gmail.com>
2021-10-01 21:49:34 +0530

    -Added success Page - added Checkout Page - Retriving the Cart on checkout page - Created model or class i.e. Orders

--- mac/shop/models.py
+++ mac/shop/models.py
@@ -25,3 +25,16 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+
+class Orders(models.Model):
+    order_id = models.AutoField(primary_key = True)
+    items_json = models.CharField(max_length = 5000, default="")
+    name = models.CharField(max_length = 100, default="")
+    email = models.CharField(max_length = 100, default="")
+    address = models.CharField(max_length = 200, default="")
+    city = models.CharField(max_length = 50, default="")
+    state = models.CharField(max_length = 50, default="")
+    zip_code = models.CharField(max_length = 50, default="")
+    phone = models.CharField(max_length = 10, default="")
+
+    
--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product, Contact
+from .models import Product, Contact , Orders
 from math import ceil
 import datetime
 
@@ -50,6 +50,7 @@ def search(request):
     return render(request, 'shop/search.html')
     # return HttpResponse("Search of Shop Page!!")
 
+
 def productView(request , myid):
     # Fetch the products using id giving name myid
     # since django create primary key by itself ie. id
@@ -60,4 +61,24 @@ def productView(request , myid):
     return render(request, 'shop/prodView.html', params)
 
 def checkout(request):
+    if request.method == "POST":
+        items_json = request.POST.get('itemsJson', '')
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        address = request.POST.get('address1', '') + " " + request.POST.get('address2', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        order = Orders(items_json = items_json
+        , name=name, email=email, address=address, city=city, state=state, zip_code=zip_code, phone=phone)
+        order.save()
+        thank = True
+        id = order.order_id
+        return render(request, 'shop/success.html', {'thank': thank , 'id' : id})
     return render(request, 'shop/checkout.html')
+
+
+def success(request):
+    return render(request, 'shop/success.html')
+    # return HttpResponse("Search of Shop Page!!")
